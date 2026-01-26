1ee71220c9cac64cf725a71a8ec4abc45039c113
LiShaojun1126 <lshaojun1126@gmail.com>
2026-01-26 00:13:46 -0500

    add reverse string

--- test/main.c
+++ test/main.c
@@ -5,12 +5,15 @@
 void swap(int *a, int *b);
 int stringLength(const char *c);
 void reverseArr(int arr[], int size);
-void reversString(char *str);
+void reverseString(char *str);
 
 
 int main(){
-    char *x = "Hello";
+    //char *x = "Hello"; //char *x is READ-ONLY!!! == const char *
+    char x[] = "Hello";
     printf("%d", stringLength(x));
+    reverseString(x);
+    printf("%s", x); //print the string
     return 0;
 }
 
@@ -40,4 +43,21 @@ int stringLength(const char *c){ //good
         c ++; //move the pointer not the char
     }
     return res;
+}
+
+void reverseString(char *str){ //good
+
+    if(str == NULL || *str == '\0') return;
+    long long size =  stringLength(str);
+    long long i = 0, j = size - 1;
+    while(i < j){
+        // char temp = *(str + i);
+        // *(str + i) = *(str + j);
+        // *(str + j) = temp;
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+
+        i++; j--;
+    }
 }
